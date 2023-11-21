3b19126e01a302194910f132571b4e9dfd22b0ea
Vova <vova.banudz@gmail.com>
2023-11-21 21:54:45 +0200

    sss

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
+// async function initMap() {
+//     // Request needed libraries.
+//     const { Map } = await google.maps.importLibrary("maps");
+//     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+//     const map = new Map(document.getElementById("map"), {
+//         center: { lat: 49.23403216617231, lng: 28.46654095998418 },
+//         zoom: 14,
+//         mapId: "4504f8b37365c3d0",
+//     });
+//     const marker = new AdvancedMarkerElement({
+//         map,
+//         position: { lat: 49.23403216617231, lng: 28.46654095998418 },
+//     });
+// }
+//
+// initMap();
 
-async function initMap() {
-    // Request needed libraries.
-    const { Map } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-    const map = new Map(document.getElementById("map"), {
-        center: { lat: 49.23403216617231, lng: 28.46654095998418 },
-        zoom: 14,
-        mapId: "4504f8b37365c3d0",
-    });
-    const marker = new AdvancedMarkerElement({
-        map,
-        position: { lat: 49.23403216617231, lng: 28.46654095998418 },
-    });
-}
-
-initMap();
+console.log(1);
