81790b83b234714ccd71fb8d47308227496c0158
Vova <vova.banudz@gmail.com>
2023-11-21 14:28:47 +0200

    map

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ async function initMap() {
     const { Map } = await google.maps.importLibrary("maps");
 
     map = new Map(document.getElementById("map"), {
-        center: { lat: -34.397, lng: 150.644 },
+        // center: { lat: -34.397, lng: 150.644 },
+        center: { lat: 49.23403216617231, lng: 28.46654095998418 },
         zoom: 8,
     });
 }
-
 initMap();
+
