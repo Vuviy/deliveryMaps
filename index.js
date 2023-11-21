e99061a843e9326481336867bbccd87137c730d6
Vova <vova.banudz@gmail.com>
2023-11-21 15:19:58 +0200

    marker

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-let map;
 
 async function initMap() {
+    // Request needed libraries.
     const { Map } = await google.maps.importLibrary("maps");
     const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
-
-    map = new Map(document.getElementById("map"), {
-        // center: { lat: -34.397, lng: 150.644 },
+    const map = new Map(document.getElementById("map"), {
         center: { lat: 49.23403216617231, lng: 28.46654095998418 },
-        zoom: 15,
+        zoom: 14,
+        mapId: "4504f8b37365c3d0",
     });
-
     const marker = new AdvancedMarkerElement({
         map,
         position: { lat: 49.23403216617231, lng: 28.46654095998418 },
     });
 }
-initMap();
 
+initMap();
